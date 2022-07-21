42ec2139b06623174888c9508e7d267f8897e41a
img98 <nichy98@naver.com>
2022-07-22 03:39:33 +0900

    미로찾기 로직 업로드

---
+++ 자료구조와 알고리즘/미로 준비/Board.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure_Algorithm
+{
+    class Board //말그대로 보드판. 현재 상황(데이터)을 담는 판
+    {
+        public TileType[,] Tile { get; private set; }// 2d좌표(인덱스)를 편하기 쓰기위해 2차원 배열을쓰자
+        public int Size { get; private set; }
+
+        public int DestY { get; private set; }
+        public int DestX { get; private set; }
+
+        Player _player; // 맵상에 움직이는 Player를 구현하기위해 새로운 클래스를 만들고 그것을 호출
+
+        public enum TileType // 빈공간과 벽을 색깔로 구분해보자. //0,1,로 구분해도되긴하는데 보기 안좋으니까..
+        {
+            Empty,
+            Wall,
+        }
+
+        public void Initialize(int size, Player player)
+        {
+            if (size % 2 == 0)  // 미로는 테두리가 벽이어야 하니까 size가 짝수면 안된다. 짝수면 길뚫기 로직이 안먹힘
+                return;
+
+            _player = player;
+
+            Tile = new TileType[size, size];
+            Size = size;
+
+            DestY = Size - 2;
+            DestX = Size - 2;
+
+            // Mazes for programmers 둘중 한 생성법 골라쓰자. 길을 뚫는 방법이 다르다.
+            // GenerateByBinaryTree(Size); // 일단 격자꼴로 empty만들고 그점들을 기준으로 우로갈지 하로갈지
+             GenerateBySideWinder(Size); // 격자로 뚫고 우하이동은 같으나, 그후 다음점이 아니라 이동한위치에서 다시 우하.
+        }
+
+        public void GenerateByBinaryTree(int size)
+        {
+            // 1. 길을 한칸 간격으로 전부 막아버리기
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++) 
+                {
+                    if (x % 2 == 0 || y % 2 == 0) // 
+                        Tile[y, x] = TileType.Wall; 
+                    else
+                        Tile[y, x] = TileType.Empty;
+                }
+            }
+            // 2.랜덤으로 우측 혹은 아래로 길 뚫기 (물론 위에서 남긴 초록점 기준으로 우,하)
+            // 50퍼 확률로 길을 뚫어보자
+            Random rand = new Random();
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0) // 1에서 뚫은 점부분은 continue
+                        continue;
+
+
+                    if (y == Size - 2 && x == Size - 2)
+                        continue; //맨 오른쪽 하단일때 벽뚫지 않게 그냥 끝내기
+                    // 외곽벽을 건드리지 않도록 해보자.
+                    if (y == Size - 2) // 타일이 미로 맨아래벽 바로 위라면
+                    {
+                        Tile[y, x + 1] = TileType.Empty; // 무조건 우측으로 가도록 설정(아래로 가면 벽뚫리니까)
+                        continue; 
+                    }
+                    if (x == Size - 2)
+                    {
+                        Tile[y + 1, x] = TileType.Empty; // 무조건 하단으로 가도록
+                        continue;
+                    }
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        Tile[y, x + 1] = TileType.Empty; // x+1 ?=  우측을 뚫는다
+                    }
+                    else // random이니 0아니면 1이 나올것
+                        Tile[y+1, x] = TileType.Empty; // 하단뚫기
+
+                }
+            }
+        }
+        public void GenerateBySideWinder(int size)
+        {
+            // 1. 길을 한칸 간격으로 전부 막아버리기
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0) // 
+                        Tile[y, x] = TileType.Wall;
+                    else
+                        Tile[y, x] = TileType.Empty;
+                }
+            }
+            // 2. 우측으로 뚫은 애들중에서 랜덤으로 하단 뚫기
+            Random rand = new Random();
+            for (int y = 0; y < Size; y++)
+            {
+                int count = 1;
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == Size - 2 && x == Size - 2)
+                        continue; //맨 오른쪽 하단일때 벽뚫지 않게 그냥 끝내기
+                    // 외곽벽을 건드리지 않게
+                    if (y == Size - 2) // 타일이 미로 맨아래벽 바로 위라면
+                    {
+                        Tile[y, x + 1] = TileType.Empty; // 무조건 우측으로 가도록 설정(아래로 가면 벽뚫리니까)
+                        continue;
+                    }
+                    if (x == Size - 2)
+                    {
+                        Tile[y + 1, x] = TileType.Empty; // 무조건 하단으로 가도록
+                        continue;
+                    }
+
+
+                    if (rand.Next(0, 2) == 0) // 우측으로 가는경우는 똑같이 뚫으면됨
+                    {
+                        Tile[y, x + 1] = TileType.Empty; // x+1 >>  우측을 뚫는다
+                        count++; // 우측으로 몇개나 뚫었는지 기억
+                    }
+                    else // 여기가 Binary와의 차이. 무조건 아래가 아니라 지금까지 뚫은점 중에서 하나에 하단
+                    {
+                        int randomIndex = rand.Next(0, count);
+                        Tile[y + 1, x - randomIndex * 2] = TileType.Empty; // 하단뚫기
+                        count = 1;
+                    }
+
+                }
+            }
+        }
+
+        const char CIRCLE = '\u25cf'; //원모양
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    // 플레이어 좌표를 갖고와서, 현재 좌표랑 일치하면 플레이어 전용색상으로 표시
+                    if (y == _player.PosY && x == _player.PosX)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    }
+                    else if (y == DestY && x == DestX) 
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    else //아니면 gettileColor
+                        Console.ForegroundColor = GetTileColor(Tile[y, x]);
+
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = prevColor; // ForegroundColor의 값을 이상하게 만들지않도록, 일 다보고 원래대로 둔다는 개념.
+        }
+        ConsoleColor GetTileColor(TileType type) // 타일 타입에 따라 다른색깔 칠해주기
+        {
+            switch(type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green; //사실 여까지 올 수가 없긴한데, 일단 그린으로두자
+            }
+        }
+    }
+}
---
+++ 자료구조와 알고리즘/미로 준비/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DataStructure_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Player player = new Player();
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board);
+
+            Console.CursorVisible = false; //커서가 0,0으로 돌아갈때 반짝이는 이펙트를 지우기위함.
+            const int WAIT_TICK = 1000 / 30; // tick의 단위가 ms이기때문에 초로 하려면 1000을 곱해줘야한다.
+            int lastTick = 0; // 프레임을 확인하기 위한, 이전전시간을 기록하는 틱
+
+            int deltaTick = 0;
+
+            while (true)
+            {
+                #region //30프레임마다 작동 구현
+                // region =별건아니고 비쥬얼스튜디오에서 열고닫을수있게 해주는 코드. 쓰면편하다.
+                int currentTick = System.Environment.TickCount; // 무언가 작동후의 시간을 뱉는 코드
+                int elapsedTick = currentTick - lastTick; // 두 틱의 차를 통해 경과시간을 구할수있겠지
+                // 만약 경과시간이 1/30초보다 작다면 
+                if (elapsedTick < WAIT_TICK) 
+                    continue; // 1/30 초마다 문을 열어서 코드를 작동하겠다. 마치 30프레임 렌더링처럼
+                deltaTick = currentTick - lastTick;
+                lastTick = currentTick;
+
+                //사실 위에도 하드코딩이 많기때문에 여러 변수설정을 통해 줄일수있다.
+                // elapsedTick을 굳이 만들지 말고 그냥 if문에 넣기.
+                // 1*1000/30 을 쓰지말고 변수로 만들기 ex) const int WAIT_TICK = 1000/30 ;
+                #endregion
+
+                Console.SetCursorPosition(0, 0); // 무조건 커서를 0,0으로 회귀시킴. 이경우 writeline을 해도 한줄이 한줄을 지우며 무한히 출력됨.
+
+                //입력 
+                //로직 (데이터가 변하는 부분)
+                player.Update(deltaTick);
+
+                //렌더링
+                board.Render();
+
+
+            }
+        }
+    }
+}
