797b3d9c43ee901851d7a86a6a28c9a80b770f89
img98 <nichy98@naver.com>
2022-08-09 01:22:28 +0900

    캐시이론, 메모리배리어 업데이트

---
+++ part4.게임서버/1.멀티쓰레드 프로그래밍/3.캐시이론_Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[10000, 10000];
+
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[y, x] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x)를 채우는데 걸린 시간 {end - now}");
+            }
+
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[x, y] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x,y)를 채우는데 걸린 시간 {end - now}");
+            }
+
+            // 위 코드 결과 아래쪽 코드의 시간이 더많이 걸린다.
+            // for문의 순서를 보면 arr[y,x]는 [1,1]->[1,2]..순이고 arr[x,y]는 [1,1]->[2,1]순임을 알수있다.
+            // 당연히 같은배열에서 두번째 칸을 보는것이, 새로운 배열을 찾는것보다 공간적으로 가까이 있을것이다.
+            // 이게 캐시의 Spacial Locality다. (공간적 지역성=명령이 들어온곳 근처의 정보를 캐시에 담는다.)
+        }
+
+
+    }
+}
+ 
---
+++ part4.게임서버/1.멀티쓰레드 프로그래밍/4.메모리배리어_Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        //메모리 재배치 = 1.코드 재배치 억제, 2. 가시성 확보
+        
+        int _answer;
+        bool _complete;
+
+        void A()
+        {
+            _answer = 123;
+            Thread.MemoryBarrier();
+            _complete = true;
+            Thread.MemoryBarrier();
+        }
+
+        void B()
+        {
+            Thread.MemoryBarrier();
+            if(_complete)
+            {
+                Thread.MemoryBarrier();
+                Console.WriteLine(_answer);
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+    }
+}
+ 
